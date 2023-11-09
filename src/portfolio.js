41e61d1748f874820d89d6f398b89f50837fdbbb
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-11-09 21:09:12 +0400

    Name fix

--- src/portfolio.js
+++ src/portfolio.js
@@ -558,7 +558,7 @@ const contactPageData = {
     title: "Blogs",
     subtitle:
       "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-    link: "https://blogs.ashutoshhathidara.com/",
+    link: "",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
